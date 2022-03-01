2ccd5fac7b7d1877fc3bfaefee38c9344563e549
nurkarakaya <karakayaanur@gmail.com>
2022-03-01 13:19:24 +0300

    one line added

---
+++ src/tr/edu/maltepe/org/example1.java
@@ -0,0 +1,13 @@
+package tr.edu.maltepe.org;
+
+public class example1 {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println('merhaba');
+
+
+
+
+    }
+}
